dafea29fdbbcec3bba262b5c68441fdab4e063cf
huangzx <498516237@qq.com>
2023-04-06 15:53:02 +0800

    1

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from typing import Optional
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from langchain import LLMChain
+from langchain.callbacks.base import AsyncCallbackManager
+from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
 from langchain.vectorstores import VectorStore
 
 import query_data
@@ -30,6 +35,71 @@ async def startup_event():
 @app.get("/")
 async def get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/all")
+async def get(request: Request):
+    return templates.TemplateResponse("index1.html", {"request": request})
+
+
+@app.websocket("/chat1")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    question_handler = QuestionGenCallbackHandler(websocket)
+    stream_handler = StreamingLLMCallbackHandler(websocket)
+    chat_history = []
+    # qa_chain = get_chain(vectorstore, question_handler, stream_handler)
+    question_gen_llm = ChatOpenAI(
+        verbose=True,
+        callback_manager=question_handler,
+    )
+    streaming_llm = ChatOpenAI(
+        streaming=True,
+        callback_manager=stream_handler,
+        verbose=True,
+        temperature=0.7,
+    )
+    manager = AsyncCallbackManager([])
+    question_generator = LLMChain(
+        llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager,
+    )
+    doc_chain = load_qa_chain(
+        streaming_llm, chain_type="stuff", prompt=query_data.QA_PROMPT, callback_manager=manager,
+    )
+    # Use the below line instead of the above line to enable tracing
+    # Ensure `langchain-server` is running
+    # qa_chain = get_chain(vectorstore, question_handler, stream_handler, tracing=True)
+
+    while True:
+        try:
+            # Receive and send back the client message
+            question = await websocket.receive_text()
+            resp = ChatResponse(sender="you", message=question, type="stream")
+            await websocket.send_json(resp.dict())
+
+            # Construct a response
+            start_resp = ChatResponse(sender="bot", message="", type="start")
+            await websocket.send_json(start_resp.dict())
+
+            result = await question_generator.acall(
+                {"question": question, "chat_history": chat_history}
+            )
+            result1 = await doc_chain.acall(result["context"])
+            chat_history.append((question, result1["answer"]))
+
+            end_resp = ChatResponse(sender="bot", message="", type="end")
+            await websocket.send_json(end_resp.dict())
+        except WebSocketDisconnect:
+            logging.info("websocket disconnect")
+            break
+        except Exception as e:
+            logging.error(e)
+            resp = ChatResponse(
+                sender="bot",
+                message="Sorry, something went wrong. Try again.",
+                type="error",
+            )
+            await websocket.send_json(resp.dict())
 
 
 @app.websocket("/chat")
